1865d34c6e80c548a8f5dd0c50a2f4cea43d142d
spankyf <deanflanagan@gmail.com>
2021-04-03 11:39:00 -0400

    Stopping at 3.36 to do React course

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action 
+from django.contrib.auth.models import User
 from .models import  Recipe, Ingredient
 from .serializers import  RecipeSerializer, IngredientSerializer
 
@@ -9,14 +10,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = RecipeSerializer
 
     @action(detail=True, methods=['POST'])
-    def add_ingredients(self, request, pk=None):
-        print(pk)
-
-        if 'ingredients' in request.data:
+    def adjust_recipe(self, request, pk=None):
+        if 'ingredients' in request.data :
             recipe = Recipe.objects.get(id=pk)
-            # print(dir(recipe))
-            # print(recipe.ingredients)
-            # print(recipe.title)
+            user = User.objects.get(id=1)
+
+            for ing in recipe.ingredients.all():
+                print(ing.name)
+            # 1 check if there's already ingredietns in there. If not, we create. If yes, we update.
+            # try:
+            #     current_ingredients = recipe.ingredients.all()
+
+
 
             response = {'message':'its working'}
             return Response(response, status=status.HTTP_200_OK)
